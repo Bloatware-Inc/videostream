985512def7fb7441116770ca110e95b704530865
Brandon Soto <brandon.soto09@gmail.com>
2017-05-30 18:58:01 -0700

    Create constants for jpg markers

--- client.py
+++ client.py
@@ -3,6 +3,9 @@ import sys
 import requests
 import numpy as np
 
+START_MARKER = b'\xff\xd8'  # start marker for jpg files
+END_MARKER = b'\xff\xd9'  # end marker for jpg files
+
 
 def stream_video():
     print('Getting stream ready...')
@@ -16,11 +19,12 @@ def stream_video():
             file_bytes = bytes()
             for chunk in response.iter_content(chunk_size=1024):
                 file_bytes += chunk
-                start_marker = file_bytes.find(b'\xff\xd8')
-                end_marker = file_bytes.find(b'\xff\xd9')
-                if start_marker != -1 and end_marker != -1:
-                    jpg = file_bytes[start_marker:end_marker + 2]
-                    file_bytes = file_bytes[end_marker + 2:]  # reset bytes
+                file_start = file_bytes.find(START_MARKER)
+                file_end = file_bytes.find(END_MARKER)
+                if file_start != -1 and file_end != -1:
+                    file_end += len(END_MARKER)  # adjust to end of file
+                    jpg = file_bytes[file_start:file_end]
+                    file_bytes = file_bytes[file_end:]  # reset bytes
                     image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)  # read jpg image
                     cv2.imshow('i', image)
                     if cv2.waitKey(1) == 27:  # escape key
