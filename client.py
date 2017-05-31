6a98355a6e41ae8854c2b5971d2e7ca91080841d
Brandon Soto <brandon.soto09@gmail.com>
2017-05-30 18:33:27 -0700

    Disable opencv GUI

--- client.py
+++ client.py
@@ -21,10 +21,11 @@ def stream_video():
                 if start_marker != -1 and end_marker != -1:
                     jpg = file_bytes[start_marker:end_marker + 2]
                     file_bytes = file_bytes[end_marker + 2:]  # reset bytes
-                    image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)  # read jpg image
-                    cv2.imshow('i', image)
-                    if cv2.waitKey(1) == 27:  # escape key
-                        exit(0)
+                    print(jpg)
+                    #image = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)  # read jpg image
+                    #cv2.imshow('i', image)
+                    #if cv2.waitKey(1) == 27:  # escape key
+                    #    exit(0)
         else:
             print("Received unexpected status code {}".format(response.status_code))
     except OSError as error:
--- run.sh
+++ run.sh
@@ -3,11 +3,6 @@
 # thanks to Jurgen:
 # https://stackoverflow.com/questions/16296753/can-you-run-gui-apps-in-a-docker-container/25280523#25280523
 
-XSOCK=/tmp/.X11-unix
-XAUTH=/tmp/.docker.xauth
-
-xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
-
 docker build -t bloatwareinc/videostream .
-docker run -ti -v $XSOCK:$XSOCK:ro -v $XAUTH:$XAUTH -e XAUTHORITY=$XAUTH -p 8081:8081 bloatwareinc/videostream
+docker run -ti -v $PWD:/videostream -p 8081:8081 bloatwareinc/videostream
 
