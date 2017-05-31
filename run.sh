6afddca3f4acf210bc3d7818bfa87b7c1ddc3586
Brandon Soto <brandon.soto09@gmail.com>
2017-05-30 18:38:18 -0700

    Add read/write permissions to mounted volume

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 # https://stackoverflow.com/questions/16296753/can-you-run-gui-apps-in-a-docker-container/25280523#25280523
 
 docker build -t bloatwareinc/videostream .
-docker run -ti -v $PWD:/videostream -p 8081:8081 bloatwareinc/videostream
+docker run -ti -v $PWD:/videostream:rw -p 8081:8081 bloatwareinc/videostream
 
